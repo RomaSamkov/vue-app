3d43b22a1134e9ee933f8e5b106a566f4d87c1b7
RomaSamkov <Romansamkov.rs@gmail.com>
2023-06-09 14:34:52 +0300

    add new project example

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
+// const App = {
+//   data() {
+//     return {
+//       CustomPlaceholder: "Enter name todo",
+//       title: "To Do List:",
+//       inputValue: "",
+//       notes: ["note1", "note2"],
+//     };
+//   },
+//   methods: {
+//     addNewNote() {
+//       if (this.inputValue !== "") {
+//         this.notes.push(this.inputValue);
+//         this.inputValue = "";
+//       }
+//     },
+//     onDeleteNote(idx) {
+//       this.notes.splice(idx, 1);
+//     },
+//     handleToUpperCase(item) {
+//       return item.toUpperCase();
+//     },
+//   },
+//   computed: {
+//     doubleCountComputed() {
+//       console.log("DoubleCount!");
+//       return this.notes.length * 2;
+//     },
+//   },
+//   watch: {
+//     inputValue(value) {
+//       if (value.length > 10) {
+//         alert("Hello Vue.JS !");
+//       }
+//     },
+//   },
+// };
+
+// Vue.createApp(App).mount("#app");
 const App = {
-  data() {
-    return {
-      CustomPlaceholder: "Enter name todo",
-      title: "To Do List:",
-      inputValue: "",
-      notes: ["note1", "note2"],
-    };
-  },
-  methods: {
-    // inputChangeHandler(e) {
-    //   this.inputValue = e.target.value;
-    // },
-    addNewNote() {
-      if (this.inputValue !== "") {
-        this.notes.push(this.inputValue);
-        this.inputValue = "";
-      }
-    },
-    onDeleteNote(idx) {
-      this.notes.splice(idx, 1);
-    },
-    handleToUpperCase(item) {
-      return item.toUpperCase();
-    },
-  },
-  computed: {
-    doubleCountComputed() {
-      console.log("DoubleCount!");
-      return this.notes.length * 2;
-    },
-  },
-  watch: {
-    inputValue(value) {
-      if (value.length > 10) {
-        alert("Hello Vue.JS !");
-      }
-    },
-  },
+  data: () => ({ title: "Grut Text" }),
+  methods: {},
+  computed: {},
+  watch: {},
 };
 
 Vue.createApp(App).mount("#app");
